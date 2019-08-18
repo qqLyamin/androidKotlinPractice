74624abe70b20f05243005411182580c177fd439
Lyamin Igor <46860477+qqLyamin@users.noreply.github.com>
2019-08-18 20:22:42 +0300

    added land

--- listners/src/main/java/com/example/listners/MainActivity.kt
+++ listners/src/main/java/com/example/listners/MainActivity.kt
@@ -16,49 +16,33 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        var TW = findViewById<TextView>(R.id.textView)
-        val Bt1 = findViewById<Button>(R.id.button1)
-        val Bt2 = findViewById<Button>(R.id.button2)
-        val Bt3 = findViewById<Button>(R.id.button3)
-
-        var flagOne = false
-
-        Bt1.setOnClickListener {
-            flagTwo = false
-            flagThree = false
-            if (!flagOne) {
-                flagOne = true
-                TW.text = "Button 1 has been pressed"
-            }
-        }
-
-        Bt2.setOnClickListener {
-            flagOne = false
-            flagThree = false
-            if (!flagTwo) {
-                flagTwo = true
-                TW.text = "Button 2 has been pressed"
-            }
-        }
-
-//        Bt3.setOnClickListener {
-//            flagTwo = false
-//            flagOne = false
-//            if (!flagThree) {
-//                flagThree = true
-//                TW.text = "Button 3 has been pressed"
-//            }
-//        }
     }
 
-    fun clickButton3(myView: View) {
-        if (findViewById<Button>(myView.id) == button3) {
-            flagTwo = false
-            flagOne = false
-            if (!flagThree) {
-                flagThree = true
-                findViewById<TextView>(R.id.textView).text = "Button 3 has been pressed"
+    fun clickButton(myView: View) {
+        when (findViewById<Button>(myView.id)) {
+            button3 -> {
+                flagTwo = false
+                flagOne = false
+                if (!flagThree) {
+                    flagThree = true
+                    findViewById<TextView>(R.id.textView).text = "Button 3 has been pressed"
+                }
+            }
+            button2 -> {
+                flagThree = false
+                flagOne = false
+                if (!flagTwo) {
+                    flagTwo = true
+                    findViewById<TextView>(R.id.textView).text = "Button 2 has been pressed"
+                }
+            }
+            button1 -> {
+                flagTwo = false
+                flagThree = false
+                if (!flagOne) {
+                    flagOne = true
+                    findViewById<TextView>(R.id.textView).text = "Button 1 has been pressed"
+                }
             }
         }
     }
